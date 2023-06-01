1c1f7ce97b00af03bc60e1c15c535b06210f2984
Daniils <daniils.miscuks@gmail.com>
2023-06-01 10:22:13 +0300

    Save

---
+++ Assets/Skripti/Objekti.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objekti : MonoBehaviour {
+	public GameObject atkritumuMasina;
+	public GameObject atraPalidziba;
+	public GameObject autobuss;
+
+	[HideInInspector]
+	public Vector2 atkrMKoord;
+	[HideInInspector]
+	public Vector2 atraPKoord;
+	[HideInInspector]
+	public Vector2 bussKoord;
+
+	public Canvas kanva;
+
+	public AudioSource audioAvots;
+	public AudioClip[] skanasKoAtskanot;
+
+	[HideInInspector]
+	public bool vaiIstajaVieta = false;
+	public GameObject pedejaisVilktais = null;
+
+
+	// Use this for initialization
+	void Start () {
+		atkrMKoord =
+		atkritumuMasina.GetComponent<RectTransform>().localPosition;
+
+       atraPKoord =
+       atraPalidziba.GetComponent<RectTransform>().localPosition;
+
+		bussKoord =
+        autobuss.GetComponent<RectTransform>().localPosition;
+    }
+}
---
+++ Assets/Skripti/Scene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Scene : MonoBehaviour {
+
+    public void Uzaskums()
+    {
+        SceneManager.LoadScene("Sakums", LoadSceneMode.Single);
+    }
+
+    //Metode,kura aizver programmu, ja ta ir izveidota ka .exe
+
+    public void Apturet()
+    {
+        Application.Quit();
+    }
+
+    //Pasiem jauztaisa metode,kas parsledzno sakuma uz UI ainu
+    public void Game()
+    {
+        SceneManager.LoadScene("PilsetasAina", LoadSceneMode.Additive);
+
+    }
+}
